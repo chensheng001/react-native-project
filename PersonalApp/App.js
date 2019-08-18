a67c9e73485aeea2d40a3082cb6ddd2bb5071b74
chensheng001 <506769376@qq.com>
2019-08-18 22:06:26 +0800

    update

---
+++ PersonalApp/App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View>
+      <Text>hello my app !</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
